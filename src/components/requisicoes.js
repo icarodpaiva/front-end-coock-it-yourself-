34d6061b4a7cc8555175a67ec73ec1ff97839e5f
icaro paiva <icaro.paiva@acct.global>
2022-07-09 23:49:54 -0300

    changed base url to requests

--- src/components/requisicoes.js
+++ src/components/requisicoes.js
@@ -1,38 +1,47 @@
 const requisicoes = {
   cadastrar: async recipe => {
-    const rawResponse = await fetch('http://localhost:2525/recipe', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(recipe)
-    })
+    const rawResponse = await fetch(
+      "https://cook-it-yourself.herokuapp.com/recipe",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(recipe)
+      }
+    )
     const content = await rawResponse.json()
 
     return content
   },
   ultima: async () => {
-    const rawResponse = await fetch('http://localhost:2525/last')
+    const rawResponse = await fetch(
+      "https://cook-it-yourself.herokuapp.com/last"
+    )
     const content = await rawResponse.json()
 
     return content
   },
   geral: async () => {
-    const rawResponse = await fetch('http://localhost:2525/recipe')
+    const rawResponse = await fetch(
+      "https://cook-it-yourself.herokuapp.com/recipe"
+    )
     const content = await rawResponse.json()
 
     return content
   },
   nome: async nome => {
-    const rawResponse = await fetch(`http://localhost:2525/name/${nome}`)
+    const rawResponse = await fetch(
+      `https://cook-it-yourself.herokuapp.com/name/${nome}`
+    )
     const content = await rawResponse.json()
 
     return content
   },
   ingredientes: async ingredientes => {
     const rawResponse = await fetch(
-      `http://localhost:2525/ingredients/${ingredientes}/`
+      `https://cook-it-yourself.herokuapp.com/ingredients/${ingredientes}/`
     )
     const content = await rawResponse.json()
 
@@ -40,21 +49,23 @@ const requisicoes = {
   },
   inregredientesRestrito: async ingredientes => {
     const rawResponse = await fetch(
-      `http://localhost:2525/ingredients/${ingredientes}/restricted`
+      `https://cook-it-yourself.herokuapp.com/ingredients/${ingredientes}/restricted`
     )
     const content = await rawResponse.json()
 
     return content
   },
   utensilios: async tools => {
-    const rawResponse = await fetch(`http://localhost:2525/tools/${tools}/`)
+    const rawResponse = await fetch(
+      `https://cook-it-yourself.herokuapp.com/tools/${tools}/`
+    )
     const content = await rawResponse.json()
 
     return content
   },
   utensiliosRestrito: async tools => {
     const rawResponse = await fetch(
-      `http://localhost:2525/tools/${tools}/restricted`
+      `https://cook-it-yourself.herokuapp.com/tools/${tools}/restricted`
     )
     const content = await rawResponse.json()
 
@@ -62,7 +73,7 @@ const requisicoes = {
   },
   time: async (timeUnit, timeQuantity) => {
     const rawResponse = await fetch(
-      `http://localhost:2525/time/${timeUnit}/${timeQuantity}/`
+      `https://cook-it-yourself.herokuapp.com/time/${timeUnit}/${timeQuantity}/`
     )
     const content = await rawResponse.json()
 
@@ -70,34 +81,39 @@ const requisicoes = {
   },
   timeRestrito: async (timeUnit, timeQuantity) => {
     const rawResponse = await fetch(
-      `http://localhost:2525/time/${timeUnit}/${timeQuantity}/restricted`
+      `https://cook-it-yourself.herokuapp.com/time/${timeUnit}/${timeQuantity}/restricted`
     )
     const content = await rawResponse.json()
 
     return content
   },
   alterar: async (id, recipe) => {
-    const rawResponse = await fetch(`http://localhost:2525/recipe/${id}`, {
-      method: 'PUT',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(recipe)
-    })
+    const rawResponse = await fetch(
+      `https://cook-it-yourself.herokuapp.com/recipe/${id}`,
+      {
+        method: "PUT",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(recipe)
+      }
+    )
     const content = await rawResponse.json()
 
     return content
   },
   id: async id => {
-    const rawResponse = await fetch(`http://localhost:2525/id/${id}`)
+    const rawResponse = await fetch(
+      `https://cook-it-yourself.herokuapp.com/id/${id}`
+    )
     const content = await rawResponse.json()
 
     return content
   },
   delete: async id => {
-    await fetch(`http://localhost:2525/recipe/${id}`, {
-      method: 'DELETE'
+    await fetch(`https://cook-it-yourself.herokuapp.com/recipe/${id}`, {
+      method: "DELETE"
     })
   }
 }
